42e8079bd227d72d4b6b4db043f49473643c210f
kruceo <kruceo.com@gmail.com>
2024-04-21 00:55:41 -0300

    wip: refactor

--- src/engine/Creator.ts
+++ src/engine/Creator.ts
@@ -1,10 +1,11 @@
 import type { CategoryChannel, Guild, PermissionsString } from "discord.js"
 import type { FSCategoryConfig } from "./interfaces/FSCategory"
-import { type ChannelSchemaPermission, type FSChannelConfig, channelSchemaPermissions } from "./interfaces/FSChannel"
+import { FSChannelConfig } from "./interfaces/FSChannel"
+import { parseSchemaPermissions } from "./permissionParser"
 
 async function createCategory(guild: Guild, config: FSCategoryConfig, perms: { [key: string]: string[] }) {
-  const parsedPerms = parseSchemaPermissions(perms, guild)
-
+  const parsedPerms = perms?parseSchemaPermissions(perms,guild):[]
+  
   const c = await guild.channels.create({
     name: config.category_name,
     type: 4, // Category Channel
@@ -20,7 +21,7 @@ async function createCategory(guild: Guild, config: FSCategoryConfig, perms: { [
 }
 
 async function createChannel(guild: Guild, config: FSChannelConfig, parentId?: string) {
-  const parsedPerms = parseSchemaPermissions(config.permissions ?? {} as any, guild)
+  const parsedPerms = config.permissions?parseSchemaPermissions(config.permissions,guild):[]
 
   let type = 0
   switch (config.type) {
@@ -54,76 +55,6 @@ async function createChannel(guild: Guild, config: FSChannelConfig, parentId?: s
  * Parse permissions of a yaml configuration file in a usable mode for Discord.js
  * If a guild is in args, this will switch the name for the role id in mentioned roles
  */
-function parseSchemaPermissions(rawPerms: { [key: string]: string[] }, guild?: Guild) {
-  let permslist = Object.entries(rawPerms) as ([ChannelSchemaPermission, string[]])[]
 
-  if (guild) {
-    const guildRoles = guild.roles.cache.map((role) => {
-      return { name: role.name, id: role.id }
-    })
-    const guildMembers = guild.members.cache.map((member) => {
-      return { name: member.user.username, id: member.id }
-    })
-
-    permslist = permslist.map((perm) => {
-      perm[1] = perm[1].map((roleCitated) => {
-
-        // try to catch any role with the role citated in the config file _perms
-        let catched = guildRoles.filter(guildRole => guildRole.name === roleCitated)[0]
-        if (catched)
-          return catched.id
-
-        // if not catched a role, this will try with guild members
-        catched = guildMembers.filter(member => member.name === roleCitated)[0]
-        if (catched)
-          return catched.id
-
-        // not catched anyone
-        return roleCitated
-      })
-      return perm
-    })
-  }
-  const separated: any = {}
-
-  // create a simple map like this { username1:{ perm1:true, perm2:true }}
-  permslist.forEach((permissionLine) => {
-    permissionLine[1].forEach((target) => {
-      if (!separated[target])
-        separated[target] = {}
-
-      const perm = abstPerm(permissionLine[0])
-      if (!perm) return;
-
-      separated[target][perm.name] = perm.value
-    })
-  })
-
-  // Return as object {target:roleOrUser,perms:AllPermsInUsableWay}
-  return Object.entries(separated).map((each) => {
-    return {
-      target: each[0],
-      perms: each[1] as Partial<Record<PermissionsString, boolean>>,
-    }
-  })
-}
-
-/**Return the perm name and if it's true or false based in the input string */
-function abstPerm(perm: string): { name: string, value?: boolean } {
-  const regxp = /(Allow|Deny|Default)(\w+)/
-  const match = perm.match(regxp)
-  if (!match) return { name: perm };
-  const [_, value, name] = match
-
-  switch (value) {
-    case "Allow":
-      return { name, value: true }
-    case "Deny":
-      return { name, value: false }
-    default:
-      return { name, value: undefined }
-  }
-} 
-
-export { createCategory, createChannel, parseSchemaPermissions }
-export default { createCategory, createChannel, parseSchemaPermissions }
+export { createCategory, createChannel }
+export default { createCategory, createChannel }
