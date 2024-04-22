84d46e79371c333de39cec49a6cbd016c979b0fd
kruceo <kruceo.com@gmail.com>
2024-04-22 08:59:35 -0300

    wip: support for custom templates path

--- src/engine/config/ambient.ts
+++ src/engine/config/ambient.ts
@@ -1,10 +1,11 @@
 import fs from "node:fs"
 import path from "node:path"
+import process from "node:process"
 
-const templatesPath = path.resolve("./templates")
+const templatesPath = path.resolve(process.env.TEMPLATES_PATH ?? "./templates")
 
 function configureAmbient() {
-  if (!fs.existsSync("./templates"))
+  if (!fs.existsSync(templatesPath))
     fs.mkdirSync(templatesPath, { recursive: true })
 }
 
